2e22f49f777a3b9f114cddd4b7d1f0a203efd171
Skylar Lea <skyniclea@gmail.com>
2024-11-06 20:52:14 -0600

    routes updated with create function - form layout

--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -1,9 +1,9 @@
 import React from "react";
-
 import { Redirect, Route, Switch } from "react-router-dom";
 import Dashboard from "../dashboard/Dashboard";
 import NotFound from "./NotFound";
 import { today } from "../utils/date-time";
+import ReservationCreate from "../reservations/ReservationCreate";
 
 /**
  * Defines all the routes for the application.
@@ -18,12 +18,19 @@ function Routes() {
       <Route exact={true} path="/">
         <Redirect to={"/dashboard"} />
       </Route>
+
       <Route exact={true} path="/reservations">
         <Redirect to={"/dashboard"} />
       </Route>
+
       <Route path="/dashboard">
         <Dashboard date={today()} />
       </Route>
+
+      <Route path="/reservations/new">
+        <ReservationCreate />
+      </Route>
+      
       <Route>
         <NotFound />
       </Route>
