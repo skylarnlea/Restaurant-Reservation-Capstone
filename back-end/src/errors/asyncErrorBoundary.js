ccc055273b00d2a776d3673dbcd58948ba43dfa6
Skylar Lea <skyniclea@gmail.com>
2024-11-07 10:09:09 -0600

    asyncErrorBoundary updated

--- back-end/src/errors/asyncErrorBoundary.js
+++ back-end/src/errors/asyncErrorBoundary.js
@@ -2,8 +2,8 @@ function asyncErrorBoundary(delegate, defaultStatus) {
     return (request, response, next) => {
       Promise.resolve()
         .then(() => delegate(request, response, next))
-        .catch((error) => {
-          const { status = defaultStatus, message = error } = error || {};
+        .catch((error = {}) => {
+          const { status = defaultStatus, message = error } = error;
           next({
             status,
             message,
@@ -11,5 +11,5 @@ function asyncErrorBoundary(delegate, defaultStatus) {
         });
     };
   }
-
-  module.export = asyncErrorBoundary;
+  
+  module.exports = asyncErrorBoundary;  
