e52169418649908a09f50b7d1be52c9e418dbd05
Skylar Lea <skyniclea@gmail.com>
2024-11-08 12:50:28 -0600

    tables list api

--- back-end/src/tables/tables.router.js
+++ back-end/src/tables/tables.router.js
@@ -3,8 +3,10 @@ const controller = require("./tables.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
 
-router.route("/")
-  .all(methodNotAllowed);
+router
+    .route("/")
+    .get(controller.list)
+    .all(methodNotAllowed);
 
 
   module.exports = router;
