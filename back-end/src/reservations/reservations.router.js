ac4f95f54d7381f64f7384f34907a27c73c36e52
Skylar Lea <skyniclea@gmail.com>
2024-11-10 13:11:53 -0600

    PUT route added for :res_id/seat

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -9,6 +9,11 @@ const controller = require("./reservations.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
 router
+    .route("/:reservation_id/seat")
+    .put(controller.update)
+    .all(methodNotAllowed);
+
+router
     .route("/:reservation_id")
     .get(controller.read)
     .all(methodNotAllowed);
