f65b7517cf3e217a4b1e913b0d33eb6cc5dffcf2
Skylar Lea <skyniclea@gmail.com>
2024-11-10 12:14:22 -0600

    fixed warning for unneeded useEffect

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -19,6 +19,7 @@ function Dashboard({ date }) {
 
   useEffect(() => {
     loadReservationsAndTables();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [date]);
 
   function loadReservations() {
