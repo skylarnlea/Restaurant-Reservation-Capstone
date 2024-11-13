356b43a93f49722a3c8b15d618ff654a4bd56e46
Skylar Lea <skyniclea@gmail.com>
2024-11-13 09:53:34 -0600

    ResForm created to simplify code -other res files updated

--- front-end/src/reservations/CancelReservationButton.js
+++ front-end/src/reservations/CancelReservationButton.js
@@ -1,50 +1,28 @@
-import React, { useState } from "react";
-import { Button, Modal } from "react-bootstrap";
+import React from "react";
 import { cancelReservation } from "../utils/api";
 
 function CancelReservationButton({ reservation_id, setReservationsError, loadReservationsAndTables }) {
-  
-    const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-    const handleOk = (event) => {
-      event.preventDefault();
-      // Want to use modal - come back to this later. //
-      const message = "Do you want to cancel this reservation? This cannot be undone.";
-      if (window.confirm(message)) {
-        cancelReservation(reservation_id, "cancelled")
-          .then(() => loadReservationsAndTables())
-          // .then(handleClose)
-          .catch(setReservationsError);
-      }
-    };
-  
-    return (
+
+  const handleOk = (event) => {
+    event.preventDefault();
+    const message = "Do you want to cancel this reservation? This cannot be undone.";
+    if (window.confirm(message)) {
+      cancelReservation(reservation_id, "cancelled")
+        .then(() => loadReservationsAndTables())
+        .catch(setReservationsError);
+    }
+  };
+
+
+  return (
     <>
       <button
         type="button"
         className="btn btn-danger"
-        onClick={handleShow}
-        data-reservation-id-cancel={reservation_id}
-      >
-        Cancel
+        onClick={handleOk}
+        data-reservation-id-cancel={reservation_id}>
+          Cancel
       </button>
-
-      {/* Modal */}
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header>
-          <Modal.Title>Do you want to cancel this reservation?</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>This cannot be undone.</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Cancel
-          </Button>
-          <Button variant="primary" onClick={handleOk}>
-            Ok
-          </Button>
-        </Modal.Footer>
-      </Modal>
     </>
   );
 }
