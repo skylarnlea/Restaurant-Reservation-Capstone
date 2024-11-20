76c2f4a57668e611b7820809ed3aa9e914aa368a
Skylar Lea <skyniclea@gmail.com>
2024-11-19 20:27:03 -0600

    updated tables migration to avoid issues

--- back-end/src/db/migrations/20241111130814_tables.js
+++ back-end/src/db/migrations/20241111130814_tables.js
@@ -14,5 +14,5 @@ exports.up = function (knex) {
   };
   
   exports.down = function (knex) {
-    return knex.schema.dropTable("tables");
+    return knex.schema.dropTableIfExists('tables'); // Use `dropTableIfExists` for safety
   };
