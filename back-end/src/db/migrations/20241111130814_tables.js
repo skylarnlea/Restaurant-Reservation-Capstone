884585bb770f0219b477f56b4bf1bf196237a9b4
Skylar Lea <skyniclea@gmail.com>
2024-11-19 20:45:06 -0600

    tables migration reverted back

--- back-end/src/db/migrations/20241111130814_tables.js
+++ back-end/src/db/migrations/20241111130814_tables.js
@@ -13,6 +13,6 @@ exports.up = function (knex) {
     });
   };
   
-  exports.down = function (knex) {
-    return knex.schema.dropTableIfExists('tables'); // Use `dropTableIfExists` for safety
-  };
+exports.down = function (knex) {
+    return knex.schema.dropTable("tables"); // Use `dropTableIfExists` for safety
+};
